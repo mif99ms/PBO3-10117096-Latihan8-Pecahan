2e288615710a592e25ba22bdf59dcfb7448f703e
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-23 18:42:35 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan8/pecahan/PBO310117096Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan8.pecahan;
+
+/**
+ *
+ * @author 
+ * NAMA         : MIFTAH MUHAMMAD SIDQI
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program untuk menampilkan data pecahan
+ */
+public class PBO310117096Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       float f;
+       double d;
+       f = 1234567890.123456789F;
+       d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
